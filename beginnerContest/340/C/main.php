94e19adc94e802fb0b5c461790e7b76dad1d3a96
orange7354 <norisio876@gmail.com>
2024-02-21 09:02:43 +0900

    340 C メモ化再起

--- beginnerContest/340/C/main.php
+++ beginnerContest/340/C/main.php
@@ -1,27 +1,34 @@
 <?php
-fscanf(STDIN, "%d",$n);
-//普通にやったらtlになる
-//メモ化再帰でやる
 
-$que = [$n];
-$count = 0;
-
-while(!empty($que)) {
-    $num = array_shift($que);
-    $count += $num;
-    $a = intdiv($num, 2);
-    $b = ceil($num/2);
-
-    if($a >= 2){
-        array_push($que, $a);
+function calculateCost($x, &$memo) {
+    // ベースケース: xが1の場合、追加のコストは発生しない
+    if ($x == 1) {
+        return 0;
     }
-
-    if($b >= 2){
-        array_push($que, $b);
+    // メモに計算済みの値が存在する場合はそれを返す
+    if (isset($memo[$x])) {
+        return $memo[$x];
     }
+    // xを2で割った商と剰余を計算
+    $half = intdiv($x, 2);
+    $isEven = $x % 2 == 0;
+    // xが偶数の場合は、両方の子が同じ値になる
+    if ($isEven) {
+        $cost = $x + 2 * calculateCost($half, $memo);
+    } else {
+        // xが奇数の場合は、2つの異なる子に対して計算
+        $cost = $x + calculateCost($half, $memo) + calculateCost($half + 1, $memo);
+    }
+    // 計算したコストをメモに保存して返す
+    $memo[$x] = $cost;
+    return $cost;
 }
 
-echo $count.PHP_EOL;
+// メイン処理
+function totalCost($N) {
+    $memo = [];
+    return calculateCost($N, $memo);
+}
 
-
-
+fscanf(STDIN, "%d", $N);
+echo totalCost($N).PHP_EOL;
