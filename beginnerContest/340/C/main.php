0485c81b0fc97bb9ee7fe72c91876515a435d642
orange7354 <norisio876@gmail.com>
2024-02-22 19:36:23 +0900

    340C復習

--- beginnerContest/340/C/main.php
+++ beginnerContest/340/C/main.php
@@ -1,34 +1,34 @@
 <?php
+fscanf(STDIN,'%d',$n);
 
-function calculateCost($x, &$memo) {
-    // ベースケース: xが1の場合、追加のコストは発生しない
-    if ($x == 1) {
+
+function caluclate($n,&$memo){
+    if($n == 1){
         return 0;
     }
-    // メモに計算済みの値が存在する場合はそれを返す
-    if (isset($memo[$x])) {
-        return $memo[$x];
+
+    if(isset($memo[$n])){
+        return $memo[$n];
     }
-    // xを2で割った商と剰余を計算
-    $half = intdiv($x, 2);
-    $isEven = $x % 2 == 0;
-    // xが偶数の場合は、両方の子が同じ値になる
-    if ($isEven) {
-        $cost = $x + 2 * calculateCost($half, $memo);
+
+    $half = intdiv($n,2);
+    $isEven = $n % 2 == 0;
+    $cost = 0;
+    if($isEven){
+        $cost += $n + 2 * caluclate($half,$memo);
     } else {
-        // xが奇数の場合は、2つの異なる子に対して計算
-        $cost = $x + calculateCost($half, $memo) + calculateCost($half + 1, $memo);
+        $cost += $n;
+        $cost += caluclate($half,$memo) + caluclate($half + 1,$memo);
     }
-    // 計算したコストをメモに保存して返す
-    $memo[$x] = $cost;
+
+    $memo[$n] = $cost;
     return $cost;
 }
 
-// メイン処理
-function totalCost($N) {
+function totalCost($n){
     $memo = [];
-    return calculateCost($N, $memo);
+    return caluclate($n,$memo);
 }
 
-fscanf(STDIN, "%d", $N);
-echo totalCost($N).PHP_EOL;
+
+echo totalCost($n).PHP_EOL;
