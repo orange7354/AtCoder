90dd3f7c4c40feb4b78a9aec1ef061ff7b608b91
orange7354 <norisio876@gmail.com>
2024-03-02 19:11:37 +0900

    ある範囲に含まれている整数の個数

--- practice/binarySearch/4/main.php
+++ practice/binarySearch/4/main.php
@@ -1,4 +1,6 @@
 <?php
+// url https://paiza.jp/works/mondai/binary_search/binary_search__basic_boss/edit?language_uid=php
+// ある範囲に含まれている整数の個数
 fscanf(STDIN, '%d', $n);
 $arr = explode(' ', trim(fgets(STDIN)));
 sort($arr);
