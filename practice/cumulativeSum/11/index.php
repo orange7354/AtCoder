202706675b6f16eca5a5fd33fcb069fbffa5edaa
orange7354 <norisio876@gmail.com>
2024-03-05 22:49:11 +0900

    区間内の個数 3

---
+++ practice/cumulativeSum/11/index.php
@@ -0,0 +1,14 @@
+<?php
+//url : https://paiza.jp/works/mondai/prefix_sum_problems/prefix_sum_problems__section_count_step3?language_uid=php
+// 区間内の個数 3
+fscanf(STDIN,'%d %d',$X,$Y);
+$arr = array_map('intval',explode(' ',trim(fgets(STDIN))));
+$memo = [0];
+foreach($arr as $idx => $a){
+    $counter = 1;
+    if($a % 2 !== 0 ){
+        $counter = 0;
+    }
+    $memo[] = $memo[$idx] + $counter;
+}
+echo $memo[$Y + 1] - $memo[$X] . PHP_EOL;
