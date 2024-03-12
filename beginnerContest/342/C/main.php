1b2013d718af8466b203e7901fb6cb96adc1882e
orange7354 <norisio876@gmail.com>
2024-03-12 21:29:20 +0900

    342 C追加

--- beginnerContest/342/C/main.php
+++ beginnerContest/342/C/main.php
@@ -1,2 +1,41 @@
 <?php
+
+function array_element_to_int($array) {
+    return array_map('intval', $array);
+}
+function get_string() {
+    return trim(fgets(STDIN));
+}
+function get_strings() {
+    return explode(' ', trim(fgets(STDIN)));
+}
+
+function get_int() {
+    return (int)trim(fgets(STDIN));
+}
+
+$N = get_int();
+$S = str_split(get_string());
+
+$Q = get_int();
+
+$alpha = range('a', 'z');
+$transformations = array_combine($alpha, $alpha);
+
+for ($i = 0; $i < $Q; $i++) {
+    list($c, $d) = get_strings();
+    foreach ($transformations as $key => $value) {
+        if ($value === $c) {
+            $transformations[$key] = $d;
+        }
+    }
+}
+
+
+$transformedString = '';
+foreach ($S as $character) {
+    $transformedString .= $transformations[$character] ?? $character;
+}
+
+echo $transformedString . PHP_EOL;
 ?>
