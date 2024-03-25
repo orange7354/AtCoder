171f81742e453125c231717f291c518e1c9b7974
orange7354 <norisio876@gmail.com>
2024-03-25 20:43:50 +0900

    add 324B

---
+++ beginnerContest/324/B/index.php
@@ -0,0 +1,14 @@
+<?php
+fscanf(STDIN,'%d',$N);
+
+for($i=0; 2 ** $i<=$N;$i++){
+    for($j=0;3**$j<=$N;$j++){
+        $result = pow(2,$i) * pow(3,$j);
+        if($result == $N){
+            echo 'Yes'.PHP_EOL;
+            exit();
+        }
+    }
+}
+
+echo 'No'.PHP_EOL;  
