9f3aad87fe47c6ba11e6b8ee7439ebae13a30f41
orange7354 <norisio876@gmail.com>
2024-04-10 21:15:08 +0900

    乗り間違い

--- practice/application_dfs_bfs/1/index.php
+++ practice/application_dfs_bfs/1/index.php
@@ -1,24 +1,34 @@
 <?php
 // url :
-fscanf(STDIN,'%d %d,%d',$N,$X,$Y);
+fscanf(STDIN,'%d %d %d',$N,$X,$Y);
 $map = array_fill(1,$N,[]);
+$degree = array_fill(1,$N,0);
 for($i = 0; $i < $N - 1; $i++){
     [$a,$b] = array_map('intval',explode(' ',trim(fgets(STDIN))));
     $map[$a][] = $b;
     $map[$b][] = $a;
-}
-$history = [];
-$visited = array_fill(1,$N,false);
-dfs($history,$visited,$X,$map);
-var_dump($history);
 
-function dfs(&$history,&$visited,$x,$map){
-    $visited[$x] = true;
-    $history[] = $x;
-    foreach($map[$x] as $val){
-        if($visited[$val] == false) {
-            dfs($history,$visited,$val,$map);
-        }   
+    $degree[$a]++;
+    $degree[$b]++;
+}
+
+$que = [$X];
+$visited = array_fill(1,$N,false);
+$dist = array_fill(1,$N,0);
+
+while(!empty($que)){
+    $node = array_shift($que);
+    foreach($map[$node] as $next){
+        if($visited[$next] === false){
+            $que[] = $next;
+            $visited[$next] = true;
+            $dist[$next] = $dist[$node] + 1;
+        }
     }
-    return;
+}
+
+if( $degree[$X] >= 2 ){
+    echo 10 + 5 * $dist[$Y] . PHP_EOL;
+} else {
+    echo 5 * $dist[$Y] . PHP_EOL;
 }
