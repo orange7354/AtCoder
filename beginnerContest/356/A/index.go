a9c7ece4fa5cce8b337031a60526f4bbe54c692d
orange7254 <norisio876@gmail.com>
2024-07-24 23:29:40 +0900

    add 356A

--- beginnerContest/356/A/index.go
+++ beginnerContest/356/A/index.go
@@ -2,29 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func numbers(size int) []int {
-	return make([]int, size)
-}
-
-func inputStrings(size int) []string {
-	return make([]string, size)
-}
-
 func main() {
-	var N, L, R int
-	fmt.Scan(&N, &L, &R)
+	var n, l, r int
+	fmt.Scan(&n, &l, &r)
 
-	for i := 1; i < L; i++ {
-		fmt.Print(i, " ")
-	}
-	for i := R; i >= L; i-- {
-		fmt.Print(i, " ")
-	}
-	for i := R + 1; i <= N; i++ {
-		fmt.Print(i, " ")
-	}
-	fmt.Println("")
+	result := make([]int, n)
 
+	for i := 0; i < n; i++ {
+		if l-1 <= i && i <= r-1 {
+			continue
+		} else {
+			result[i] = i + 1
+		}
+	}
+
+	insert := make([]int, r-l+1)
+	for i := 0; i < r-l+1; i++ {
+		insert[i] = i + l
+	}
+
+	sort.Slice(insert, func(i, j int) bool { return insert[i] > insert[j] })
+
+	for i, v := range insert {
+		result[l-1+i] = v
+	}
+
+	for _, v := range result {
+		fmt.Printf("%d ", v)
+	}
 }
