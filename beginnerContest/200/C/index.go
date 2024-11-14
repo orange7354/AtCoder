f1185567943909cdc631cceff878f0cf4485e980
orange7254 <norisio876@gmail.com>
2024-11-14 22:56:35 +0900

    r 200 C

--- beginnerContest/200/C/index.go
+++ beginnerContest/200/C/index.go
@@ -1,22 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var n int
-	var a []int
 	fmt.Scan(&n)
 
-	a = make([]int, n)
-
+	a := make([]int, n)
 	for i := 0; i < n; i++ {
 		fmt.Scan(&a[i])
 	}
 
+	//余りをカウントするスライス
 	cnt := make([]int, 200)
-	var ans int = 0
-	for j := 0; j < n; j++ {
-		r := a[j] % 200
+	ans := 0
+	for _, v := range a {
+		r := v % 200
 		ans += cnt[r]
 		cnt[r]++
 	}
